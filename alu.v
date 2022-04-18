b66c11166f8ff77eebafbb65c120eba202ebb431
QuaATran <98537228+QuaATran@users.noreply.github.com>
2022-04-18 14:12:49 -0400

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,17 +1,20 @@
-module alu (out, in1, in2, S, CO);
+module alu (out, in1, in2, S, Z, C, N, V);
 
  input  [2:0]  S; //select bit
  input  [31:0] in1, in2; //32 bit inputs
+ output Z, C, N, V;
  output [31:0] out; //output 
  reg 	  [32:0] result; //overflow bit checker; we make this 33 bit bc the 33rd bit is overflow, 32 bit will not see an overflow bit
  // tried using if in1[31] == 1 and in[31] == 1, but problem arises when there is overflow but they are 0 therefore made 33 bit to read an overflow bit
  
- output CO; // overflow bit
- 
  assign out = result; // making output only be 32 bits
  
  //overflow bit is on if the most significant bit are 1
- assign CO = result[32];
+ assign V = result[32];
+ assign Z = (out == 32'b0) ? 1 : 0;
+ assign N = out[31];
+ assign C = result[32];
+ 
   
  
  always @(*) begin
